eb3af38b71fa184721a2352577cd8d0627dc4f9d
Sophie Lin <sophielin.tm@gmail.com>
2020-10-06 19:13:38 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -5,5 +5,19 @@ public class Hello {
 //        System.out.println("Hello World");
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 19;
+        //用"參照物件型態"的Integer類型，代表將它視為一個物件，就可以使用java裡的功能
+        //如age2.byteValue()
+        //這種又稱作 Wrapper Class 包裝型態
+        char c = '我';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 66.5f;
+        float height = 1.7f;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Andrei";
+        //Reference data type參照資料型態，首字大寫，如String, Integer等
     }
 }
--- src/main/kotlin/com/tom/Person.kt
+++ src/main/kotlin/com/tom/Person.kt
@@ -4,8 +4,15 @@ fun main() {
 //    println("Hello World")
 //    Human().hello()
         val h = Human()
-        h.hello()
-    // 這裡用var跟val(value)都可以，val代表建立後不能再更動值
+        //h.hello()
+    // 這裡用var跟val(value)都可以，val代表指定後不能再改變
+        val age = 19
+        val age2 : Int = 19
+    //Kotlin的"自動推斷變數型態"，所以不指定資料型態
+        var weight : Float = 66.5f
+    //Kotlin跟Java一樣小數點預設都是 Double型態
+        var name : String
+        name = "Andrei"
 }
 class Human {
     fun hello(){
